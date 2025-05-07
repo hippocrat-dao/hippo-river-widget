1b238c7777d8240729f6bd28a1ed9fee31ab1595
kek <0114kek@snu.ac.kr>
2025-05-07 15:53:07 +0900

    fix: handle account for sequence 0

--- lib/components/TxDialog/index.vue
+++ lib/components/TxDialog/index.vue
@@ -141,6 +141,7 @@ async function initData() {
         view.value = 'input';
         p.value = JSON.parse(props.params || '{}');
         getAccountInfo(props.endpoint, props.sender).then((res) => {
+            if (!res.info?.pub_key) return;
             memo.value = `did:hp:${base64ToHex(res.info.pub_key.key)}`;
         });
         const fee = convert.baseToUnit(p.value?.fees || { amount: '1', denom: 'hp' }, 'hp') // 1HP for default fee
